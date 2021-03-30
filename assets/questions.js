16fb6b79e3658703c666d75888fee79f8c61497a
mascarelloa <amfm0793@gmail.com>
2021-03-30 14:20:32 -0400

    update to javascript.

--- assets/questions.js
+++ assets/questions.js
@@ -1,32 +1,69 @@
 var questions = [
     {
-        title: "question 1",
-        choice: ["choice 1", "choice 2", "choice 3", "choice 4"],
-        answer: "choice 2 ",
+        title: "In HTML, what tag is used to define Javascript?",
+        choice: ["<js></js>", "<script></script>", "<javascript></javascript>", "<java></java>"],
+        answer: "<script></script>",
     },
     
     {
-        title: "question 2",
-        choice: ["choice 1", "choice 2", "choice 3", "choice 4"],
-        answer: "choice 2 ",
+        title: "How do you trigger an alert?",
+        choice: ["alert('This is an alert!');", "msg('This is an alert!');", "alert[This is an alert!]", "popup('This is an alert');"],
+        answer: "alert('This is an alert!');",
     },
     
     {
-        title: "question 3",
-        choice: ["choice 1", "choice 2", "choice 3", "choice 4"],
-        answer: "choice 2 ",
+        title: "How do you call a function named 'myFunction'?",
+        choice: ["call myFunction; ", "myFunction{}", "myFunction()", "call function(myFunction);"],
+        answer: "myFunction()",
     },
     
     {
-        title: "question 3",
-        choice: ["choice 1", "choice 2", "choice 3", "choice 4"],
-        answer: "choice 2 ",
+        title: "What are the logical operators used in Javascript?",
+        choice: ["+, -, *, and /", "&&, ||, and !", "=, <, and >", "x, y, and z"],
+        answer: "&&, ||, and !",
     },
     
     {
-        title: "question 5",
-        choice: ["choice 1", "choice 2", "choice 3", "choice 4"],
-        answer: "choice 2 ",
+        title: "How do you add comments in Javascript?",
+        choice: ["<!--Your comment here-->", "*/Your comment here/*", "You cannot make comments in Javascript", "//Your comment here"],
+        answer: "//Your comment here",
     
-    }]
+    },
+
+    {
+        title: "What is an array?",
+        choice: ["A list of integers.", "A list of strings.", "Multiple values stored in a single value.", "A very long function."],
+        answer: "Multiple values stored in a single value.",
+    
+    },
+
+    {
+        title: "What function tests whether a value is true or false?",
+        choice: ["isNaN()", "isEqual()", "truthy() or falsy()", "Boolean()"],
+        answer: "Boolean()",
+    
+    },
+
+    {
+        title: "What code returns a random number between 0 and 1?",
+        choice: ["math.random()", "Math.random()", "Math.floor(Math.random())", "random.math()"],
+        answer: "Math.random()",
+    
+    },
+
+    {
+        title: "What method removes the last element from an array?",
+        choice: ["pop()", "concat()", "push()", "unshift()"],
+        answer: "pop()",
+
+    },
+
+    {
+        title: "What method returns a number into a string?",
+        choice: ["createString()", "stringThis()", "toString()", "stringy()"],
+        answer: "toString()",
+
+    }
+
+]
     
--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,63 @@
-var currentQuestionIndex = 0;
+// These are the globally declared variables.
+var startButton = document.querySelector("#start");
+var questionScreen = document.querySelector("#questions");
+var questionPool = 0;
+var theTimer = document.querySelector("#time");
 var time = questions.length * 15;
+var theChoices = document.querySelector("#choices");
 
-var startBtn = document.querySelector("#start");
-var questionsElement = document.querySelector("#questions");
-var timerElement = document.querySelector("#time");
-var questionChoices = document.querySelector("#choices");
-
-
-
+// This will start the quiz when the Start button is clicked by making the start screen hide and the questions show.
+startButton.onclick = startQuiz;
 
 function startQuiz() {
-    var startScreen = document.querySelector("#start-screen");
-    startScreen.setAttribute('class', "hide");
-    questionsElement.removeAttribute("class");
-    getCurrentQuestion()
-
+  var startScreen = document.querySelector("#start-screen");
+  startScreen.setAttribute("class", "hide");
+  questionScreen.removeAttribute("class");
+  getQuestion();
 }
 
 
+// This will pull and display a current question's title and choices.
+function getQuestion() {
+  var currentQuestion = questions[questionPool];
+  var questionTitle = document.querySelector("#question-title");
+  questionTitle.textContent = currentQuestion.title;
+  
+  for (var i = 0; i < currentQuestion.choice.length; i++) {
+    var userChoice = document.createElement("button");
+    userChoice.setAttribute("class", "choice");
+    userChoice.setAttribute("value", currentQuestion.choice[i]);
+    userChoice.textContent = i + 1 + ". " + currentQuestion.choice[i];
+    theChoices.appendChild(userChoice);
 
+    userChoice.onclick = checkAnswer;
+  }
 
+    function checkAnswer(event) {
+    var userAnswer = event.target.value;
+    console.log(userAnswer);
 
-// for (var i = 0; i < questions.length; i++) {
-
-    function getCurrentQuestion() {
-        var currentQuestion = questions[currentQuestionIndex];
-        var titleElement = document.querySelector("#question-title")
-        titleElement.textContent = currentQuestion.title;
+    if (userAnswer === questions[questionPool].answer) {
+      alert("Correct!");
+    } else {
+      alert("Incorrect!");
+    }  
     
-        console.log(currentQuestion.title);
-        for (var i = 0; i < currentQuestion.choice.length; i++) {
-            var userChoice = document.createElement("button");
-            userChoice.setAttribute("class", "choice")
-            userChoice.setAttribute("value", currentQuestion.choice[i])
-            userChoice.textContent = i + 1 + ". " + currentQuestion.choice[i];
-            questionChoices.appendChild(userChoice);
-        }
-    }
+    theChoices.innerHTML="";
 
+    questionPool++;
 
-    // // 
-    // if (userAnswer === currentQuestion.answer) {
-    //     // add point to score and move to next question
-    // } else {
-    //     //decrement time, and move to next question.
-    // }
+    getQuestion();
+  }
+}
+// This will compare the user's choice with the answer and decide if it is incorrect or correct.
 
-   
+// if (userAnswer === currentQuestion.answer) {
+//     console.log("True")
+// display correct message
+// add point to score and move to next question
+// } else {
+//display incorrect message.
+//decrement time, and move to next question.
 
 // }
- 
-startBtn.onclick = startQuiz;
